99c3ff0fcc718d357871ed6d4c5552d60d7d7d69
姚头 <632319345@qq.com>
2022-07-22 19:07:44 +0800

    init

---
+++ src/pages/mine/mine.vue
@@ -0,0 +1,16 @@
+<template>
+  <view class="content"> 我的 </view>
+</template>
+
+<script setup lang="ts">
+console.log(
+  '%c import.meta.env.VITE_SOME_KEY' + import.meta.env.VITE_SOME_KEY,
+  'color: red'
+);
+</script>
+
+<style lang="scss">
+.content {
+  background: red;
+}
+</style>
